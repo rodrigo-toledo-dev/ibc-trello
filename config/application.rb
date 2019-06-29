eba3b7cdd7b97e4410205f73af57de06636b187c
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-06-29 00:06:34 -0300

    configuring editorconfig moving home controller to boards internacionalization for application and menu

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,7 @@ module IbcTrello
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :'pt-BR'
   end
 end
