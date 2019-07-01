6bd1e972505237256b8f697607e6e634c9dd722f
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-07-01 19:26:03 -0300

    updating the steps with success

---
+++ app/controllers/steps_controller.rb
@@ -0,0 +1,28 @@
+class StepsController < ApplicationController
+  def update
+    @step = Step.friendly.find(params[:id])
+    if @step.update_attributes(step_params)
+      flash[:success] = 'Etapa atualizada com sucesso'
+    else
+      flash[:error] = "Erro ao atualizar esta Etapa, verifique os alertas: #{@step.errors.full_messages.to_sentence}"
+    end
+    redirect_to board_path(@step.board)
+  end
+
+  def destroy
+    @step = Step.friendly.find(params[:id])
+    if @step.destroy
+      flash[:success] = 'Etapa removida com sucesso'
+    else
+      flash[:error] = "Erro ao remover esta Etapa"
+    end
+    redirect_to root_path
+  end
+
+  protected
+
+  def step_params
+    params.require(:step).permit(:name)
+  end
+
+end
